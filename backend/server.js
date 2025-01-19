1623afe71b65173b894db09fade7cfac099c511e
esitorsunil <sunilrio23@gmail.com>
2025-01-19 21:58:24 +0530

    MongoDB connect to backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoute from "./routes/auth.route.js";
+import connectDB from "./DB/connectDB.js";
 
 dotenv.config()
 const app = express();
@@ -12,4 +13,5 @@ app.use("/api/auth", authRoute)
 
 app.listen(PORT, () => {    
     console.log(`Server running on port ${PORT}`);
+    connectDB();
 });
