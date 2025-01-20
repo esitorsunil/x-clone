97ba42bf01a55641312402672ea14b25dfb8e2f5
esitorsunil <sunilrio23@gmail.com>
2025-01-21 00:02:47 +0530

    follow& unfollow (user controller)

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -15,3 +15,39 @@ export const getProfile = async(req, res) => {
         res.status(500).json({error: "Internal Server Error"});
     }
 }
+
+export const followUnfollowUser = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const userToModify = await User.findById({_id: id});
+        const currentUser = await User.findById({_id: req.user._id});
+
+        if(id === req.user._id) {
+            return res.status(400).json({error: "You cannot follow yourself"});
+        }
+
+        if(!userToModify || !currentUser) {
+            return res.status(400).json({error: "User not found"});
+        }
+
+        const isFollowing = currentUser.following.includes(id);
+
+        if(isFollowing) {
+            //unfollow
+            await User.findByIdAndUpdate ({_id: id}, {$pull: {followers: req.user._id}});
+            await User.findByIdAndUpdate ({_id: req.user._id}, {$pull: {following: id}});
+            res.status(200).json({message: "Unfollow successfully"});
+        }
+        else {
+            //follow
+            await User.findByIdAndUpdate ({_id: id}, {$push: {followers: req.user._id}});
+            await User.findByIdAndUpdate ({_id: req.user._id}, {$push: {following: id}});
+            //send notification
+            res.status(200).json({message: "Follow successfully"});
+        }
+
+    } catch (error) {
+        console.log(`Error in follow and Unfollow controller: ${error}`);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProfile } from "../controllers/user.controller.js";
+import { getProfile, followUnfollowUser} from "../controllers/user.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getProfile)
+router.post("/follow/:id", protectRoute, followUnfollowUser)
 
 export default router;
