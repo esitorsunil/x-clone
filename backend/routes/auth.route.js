2a2efb1e8f57d08799a2ac439e88b3b2fa7c0fb9
esitorsunil <sunilrio23@gmail.com>
2025-01-20 22:30:20 +0530

    get Profile controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controllers.js";
+import { getMe, login, logout, signup } from "../controllers/auth.controllers.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/me", protectRoute, getMe)
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 //const express = require("express");
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoute from "./routes/auth.route.js";
 import connectDB from "./DB/connectDB.js";
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute)
 
