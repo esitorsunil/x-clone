c9a52e807c8063678a3ce12df2468729be87f560
esitorsunil <sunilrio23@gmail.com>
2025-01-23 21:04:37 +0530

    Delete post (controller)

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -58,3 +58,34 @@ export const deletePost = async(req, res) => {
         res.status(500).json({error: "Internal Server Error"});
     }
 }
+
+export const createComment = async(req, res) => {
+    try {
+        const {text} = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+
+        if (!text) {
+            return res.status(400).json({ error: "Comment text is required" });
+          }
+
+        const post = await Post.findOne({_id: postId});
+        if(!post){
+            return res.status(404).json({error: "Post not found"});
+        }
+
+        const comment = {
+            user: userId,
+            text
+        }
+
+        post.comments.push(comment);
+        await post.save();
+        res.status(200).json(post);
+        
+    } catch (error) {
+        console.log(`Error in createComment controller: ${error}`);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}   
+
