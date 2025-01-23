76cd2d6e9bd7f9dd3b96b548ac583be0881c3c6e
esitorsunil <sunilrio23@gmail.com>
2025-01-24 00:07:06 +0530

    get all post (controller)

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -127,3 +127,17 @@ export const likeUnlikePost = async(req, res) => {
         res.status(500).json({error: "Internal Server Error"});
     }
         }
+
+export const getAllPosts = async(req, res) => {
+    try {
+        const posts = await Post.find({}).sort({createdAt: -1});
+        if(posts.length === 0){
+            return res.status(404).json([]);
+        }
+        res.status(200).json(posts);
+
+    } catch (error) {
+        console.log(`Error in getAllPosts controller: ${error}`);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { createPost, deletePost, createComment, likeUnlikePost } from "../controllers/post.controller.js";
+import { createPost, deletePost, createComment, likeUnlikePost, getAllPosts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
+router.get("/all" , protectRoute, getAllPosts);
 router.post("/create" , protectRoute, createPost);
 router.post("/like/:id" , protectRoute, likeUnlikePost);
 router.post("/comment/:id" , protectRoute, createComment);
