ac267def4723e9ec40e01d86e27b3cbfa3a0cc19
esitorsunil <sunilrio23@gmail.com>
2025-01-26 22:59:22 +0530

    setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl'>The is the app</h1>
+      <button className="btn btn-outline btn-success">Success</button>
+      </div>
+
+  )
+}
+
+export default App
